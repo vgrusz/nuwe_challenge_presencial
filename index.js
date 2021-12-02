a503d456c54fe406d845257750599d9db885ad18
victor <vgrusz@yahoo.com.ar>
2021-12-02 15:32:45 +0100

    Added comments and error handling

--- index.js
+++ index.js
@@ -9,29 +9,40 @@ const PORT = process.env.PORT ? process.env.PORT : 3001;
 app.use(express.json());
 
 app.post("/process", (req, res) => {
-  let squareDeltaSummation = 0;
-  let n = testTarget.length;
+  try {
+    let squareDeltaSummation = 0;
+    let n = testTarget.length;
 
-  let inputFromFrontend = req.body;
+    let inputFromFrontend = req.body;
 
-  for (i = 0; i < testTarget.length; i++) {
-    let delta = testTarget[i] - inputFromFrontend[i];
-    let squareDelta = delta * delta;
-    squareDeltaSummation += squareDelta;
-    //console.log(delta, " ", squareDelta);
+    //Verifies incoming data quantity
+    if (inputFromFrontend.length != 990) throw new Error("Length of input not equal to 990");
+
+    for (i = 0; i < testTarget.length; i++) {
+      //verifies incoming data type
+      if (typeof testTarget[i] != "number") throw new Error("Type error, expected number, received other");
+      if (typeof inputFromFrontend[i] != "number") throw new Error("Type error, expected number, received other");
+
+      let delta = testTarget[i] - inputFromFrontend[i];
+      let squareDelta = delta * delta;
+      squareDeltaSummation += squareDelta;
+    }
+
+    let ecm = (1 / n) * squareDeltaSummation;
+
+    console.log("Summation of delta's squares * 1/n", squareDeltaSummation * (1 / n));
+
+    res.send({
+      ecm: ecm,
+      prediccion: inputFromFrontend,
+      objetivo: testTarget,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({ "error ": error.message });
   }
-
-  let ecm = (1 / n) * squareDeltaSummation;
-
-  console.log("sumatoria deltas al cuadrado * 1/n", squareDeltaSummation * (1 / n));
-
-  res.send({
-    ecm: ecm,
-    prediccion: inputFromFrontend,
-    objetivo: testTarget,
-  });
 });
 
 app.listen(PORT, () => {
-  console.log("app escuchando puerto ", PORT);
+  console.log("app listening on port ", PORT);
 });
